459b987254585c5e5ef58a303e00a981261ccbf1
izumookuni <izumookuni@outlook.com>
2020-05-25 09:37:55 +0800

    new features

--- src/main/java/cc/domovoi/spring/controller/paging/GeneralPagingControllerInterface.java
+++ src/main/java/cc/domovoi/spring/controller/paging/GeneralPagingControllerInterface.java
@@ -6,10 +6,7 @@ import cc.domovoi.spring.service.paging.GeneralPagingServiceInterface;
 import cc.domovoi.spring.utils.RestfulUtils;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -39,7 +36,7 @@ public interface GeneralPagingControllerInterface<K, E extends GeneralJoiningEnt
             method = {RequestMethod.POST},
             produces = "application/json")
     @ResponseBody
-    default Map<String, Object> findPagingList(@RequestBody E entity, Integer pageNum, Integer pageSize, List<String> sortBy, List<String> sortOrder) {
+    default Map<String, Object> findPagingList(@RequestBody E entity, @RequestParam(required = false) Integer pageNum, @RequestParam(required = false) Integer pageSize, @RequestParam(required = false) List<String> sortBy, @RequestParam(required = false) List<String> sortOrder) {
         Map<String, Object> jsonMap = new HashMap<>();
         Integer total = findCountFunction(entity);
         List<E> entityList = findPagingListFunction(entity, pageNum, pageSize, sortBy, sortOrder);
